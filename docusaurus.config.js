bd3b0bfbbfccd7581d72d91523ff4226243cf1dd
Multyp <miguel.dasilva2004@hotmail.com>
2024-03-11 16:50:45 +0000

    edit: updated docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ubblot.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ubblot', // Usually your GitHub org/user name.
+  projectName: 'ubblot.com', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
